a11f9fbaac8b9ce9cc1dd74a47273c207175866b
asgrom <as.grom@gmail.com>
2019-04-27 20:39:02 +0300

    some change

--- seo/console_menu.py
+++ seo/console_menu.py
@@ -1,4 +1,4 @@
-from colorama import init, Fore, Back
+from colorama import init, Fore, Back, Style
 
 init(autoreset=True)
 
@@ -9,13 +9,17 @@ def main_menu():
     print(Fore.LIGHTGREEN_EX + Back.BLACK + f'{"":-<94}')
     print(Back.BLACK + f'{"":<94}')
     print(Fore.LIGHTGREEN_EX + Back.BLACK + f'{"":<4}{"[1] ПОИСКОВАЯ ФРАЗА":<40}{"[5] ПОИСКОВИК":<50}')
-    print(Fore.LIGHTGREEN_EX + Back.BLACK + f'{"":<4}{"[2] URL ВЕБ-САЙТА":<40}{"[6] НАЧАТЬ ПРОСМОТР С ПОИСКОВИКА":<50}')
+    print(
+        Fore.LIGHTGREEN_EX + Back.BLACK + f'{"":<4}{"[2] URL ВЕБ-САЙТА":<40}'
+        f'{Fore.LIGHTBLUE_EX + Style.BRIGHT}{"[6] НАЧАТЬ ПРОСМОТР С ПОИСКОВИКА":<50}')
     print(
         Fore.LIGHTGREEN_EX + Back.BLACK + f'{"":<4}{"[3] МЕСТОПОЛОЖЕНИЕ (только для ЯНДЕКС)":<40}'
         f'{"[7] ПРОСМОТР ДАННЫХ ЗАПРОСА":<50}')
 
     print(Fore.LIGHTGREEN_EX + Back.BLACK + f'{"":<4}{"[4] ТАЙМЕР":<40}{"[8] ПРОСМОТР АДРЕСОВ ПОСЕЩЕННЫХ СТРАНИЦ":<50}')
-    print(Back.BLACK + Fore.LIGHTGREEN_EX + f'{"":<44}{"[9] НАЧАТЬ ПРОСМОТР ЛИНКОВ НА АКТИВНОЙ СТРАНИЦЕ":<50}')
+    print(
+        Back.BLACK + Fore.LIGHTGREEN_EX + f'{"":<44}'
+        f'{Fore.LIGHTBLUE_EX + Style.BRIGHT}{"[9] НАЧАТЬ ПРОСМОТР ЛИНКОВ НА АКТИВНОЙ СТРАНИЦЕ":<50}')
     print(Fore.LIGHTGREEN_EX + Back.BLACK + f'{"":<4}{"[Q] ВЫХОД":<90}')
     print(Back.BLACK + f'{"":<94}')
 
